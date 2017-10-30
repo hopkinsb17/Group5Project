6ccb9f1f9b0020f93aadc6345b4399413501252a
Dillon Paulus <paulusd17@students.ecu.edu>
2017-10-30 08:32:20 -0400

    Uploading Ancy index with the login page

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+<body>
+
+<h1>Hello Team</h1>
+<p>This one was created using external css.</p>
+<div>
+<?php
+include('HomePage.php');
+?>
+</div>
+
+</body>
+</html>
